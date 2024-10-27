8ca3050bd8aea325b0421bb42398c02739677ffe
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-10-27 21:37:01 +0200

    Added extra test cases

--- tests/src/tests/mod.rs
+++ tests/src/tests/mod.rs
@@ -5,4 +5,7 @@ fn test_ceasar_cipher() {
     let result = caesar_cipher!("Hello, World!", 3);
     let expected = "Khoor, Zruog!";
     assert_eq!(result, expected);
+    assert_eq!("", caesar_cipher!("", 3));
+    assert_ne!("a", caesar_cipher!("a", 3));
+    assert_eq!("a", caesar_cipher!("a", 0));
 }
